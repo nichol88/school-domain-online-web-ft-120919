1fa877a85aff729aa986bfb2f32b6d5657d0e7fa
Brian Nicholls <nichol88@gmail.com>
2019-12-11 07:42:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !@roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.map{ |k, v|
+      puts "B4: #{v}"
+      v.sort!
+      puts "aft #{v}"
+    }
+    @roster
+  end
+end
